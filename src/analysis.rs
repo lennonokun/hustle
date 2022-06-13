a9e200189b390247ebb4483bbc16276a4294d200
Lennon Okun <lennonokun@gmail.com>
2022-06-13 11:50:10 -0400

    combined htable and hdata

--- src/analysis.rs
+++ src/analysis.rs
@@ -3,23 +3,66 @@ use std::io::{BufRead, BufReader, Write, Result};
 
 use crate::ds::NWORDS;
 
-pub type HCache = [f64; NWORDS+1];
+#[derive(Debug)]
+pub struct HData {
+	// let ht[0] be zero to make indexing easier, so +1
+	// loaded data
+	approx: [f64; NWORDS+1],
+	// recorded data
+	rsums: [f64; NWORDS+1],
+	rcts: [i64; NWORDS+1],
+	rinfs: [i64; 7]
+}
+
+impl HData {
+	pub fn new() -> Self {
+		HData{
+			approx:[0.0; NWORDS+1],
+			rsums: [0.0; NWORDS+1],
+			rcts: [0; NWORDS+1],
+			rinfs: [0; 7]
+		}
+	}
+
+	pub fn record(self: &mut Self, n: usize, eval: f64) { 
+		self.rsums[n] += eval;
+		self.rcts[n] += 1;
+	}
+
+	pub fn record_inf(self: &mut Self, n: usize) {
+		self.rinfs[n] += 1;
+	} 
+
+	#[inline]
+	pub fn get_approx(self: &Self, n: usize) -> f64 {
+		self.approx[n]
+	}
+	
+	// read approximated data from file
+	pub fn read(self: &mut Self, p: &str) -> Result<()> {
+		let file = File::open(p)?;
+		let reader = BufReader::new(file);
+		self.approx = reader.lines()
+			.filter_map(|s| s.ok()?.parse::<f64>().ok())
+			.collect::<Vec<f64>>()
+			.try_into().expect("expected NWORDS+1 lines in heuristic cache");
+		Ok(())
+	}
+
+	// write recorded data to file
+	pub fn write(self: &mut Self, path: &str) -> Result<()> {
+		let mut out = File::create(path)?;
+		writeln!(&mut out, "x,y,ct");
+		for i in 0..NWORDS+1 {
+			let y = if self.rcts[i] == 0 {0.0}
+			else {self.rsums[i] / self.rcts[i] as f64};
+
+			writeln!(&mut out, "{},{},{}", i, y, self.rcts[i]);
+		}
+		Ok(())
+	}
+}
 
 // pub fn write_data(path: &str, data: &Vec<(f64, f64)>) -> Result<()> {
-	// let mut out = File::create(path)?;
-	// writeln!(&mut out, "x,y");
-	// for (x,y) in data {
-		// writeln!(&mut out, "{},{}", x, y);
-	// }
-	// Ok(())
 // }
 
-// make heuristic cache from file
-pub fn read_heuristic(p: &str) -> Result<HCache> {
-	let file = File::open(p)?;
-	let reader = BufReader::new(file);
-	Ok(reader.lines()
-		 .filter_map(|s| s.ok()?.parse::<f64>().ok())
-		 .collect::<Vec<f64>>()
-		 .try_into().expect("expected 2316 lines in heuristic cache"))
-}
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,19 @@ mod ds;
 use crate::ds::{Word, Feedback, DTree, WSet, FbMap, get_words};
 
 mod analysis;
-use crate::analysis::{HTable, read_heuristic};
+use crate::analysis::HData;
+
+const NTOPS: usize = 15;
+const ENDGCUTOFF: usize = 25;
 
 // TODO:
 // multithread
 // better understand borrowing
 // heuristic is worse now for bigger partitions bc i dont sample enough
+// are inf counts useful?
+// keep track of n for hdata?
+// is it incorrect for happrox.csv to give 1.0 for x=1?
+// why cant i get past 3.425?
 
 // get feedback partitions
 fn fb_partition(gw: Word, aws: &WSet) -> FbMap<WSet> {
@@ -34,17 +41,17 @@ fn fb_counts(gw: Word, aws: &WSet) -> FbMap<i32> {
 }
 
 // apply precalculated heuristic to partition sizes (lower is better)
-fn heuristic(gw: Word, aws: &WSet, ht: &HTable) -> f64 {
+fn heuristic(gw: Word, aws: &WSet, hd: &HData) -> f64 {
 	fb_counts(gw, aws).iter()
-		.map(|(_, n)| ht[*n as usize])
+		.map(|(_, n)| hd.get_approx(*n as usize))
 		.sum()
 }
 
 // get top n words based off of heuristic
-fn top_words(gws: &WSet, aws: &WSet, ht: &HTable, n: usize)
+fn top_words(gws: &WSet, aws: &WSet, hd: &HData, n: usize)
 	-> Vec<Word> {
 	let mut tups : Vec<(Word, f64)> = gws.iter()
-		.map(|gw| (*gw, heuristic(*gw, aws, ht)))
+		.map(|gw| (*gw, heuristic(*gw, aws, hd)))
 		.collect();
 	tups.sort_by(|(_, f1), (_, f2)| f1.partial_cmp(f2).unwrap());
 	tups.iter()
@@ -74,22 +81,25 @@ fn top_words(gws: &WSet, aws: &WSet, ht: &HTable, n: usize)
 
 // get upper bound for minimum mean guesses at state given guess
 fn solve_given(gw: Word, gws: &WSet, aws: &WSet,
-							n: i32, ht: &HTable) -> Option<DTree> {
+							 n: i32, hd: &mut HData) -> Option<DTree> {
 	// unnecessary unless user is dumb
 	// if aws.len() == 1 && gw == aws.iter().next().unwrap() {
 		// return 0.0;
 	// }
 	// todo if n == 1 && aws.len() > 20?
-	if n == 0 {return None}
+	if n == 0 {
+		return None
+	}
 
 	let mut eval = 1.0;
 	let mut fbmap = FbMap::new();
 	for (fb, set) in fb_partition(gw, aws) {
 		if !fb.is_correct() {
-			let dt2 = solve_state(gws, &set, n-1, ht);
+			let dt2 = solve_state(gws, &set, n-1, hd);
 			match dt2 {
-				None => return None,
-				Some(dt2) => {
+				None => {
+					return None;
+				} Some(dt2) => {
 					eval += (set.len() as f64/aws.len() as f64) * dt2.get_eval();
 					fbmap.insert(fb, dt2);
 				}
@@ -103,8 +113,7 @@ fn solve_given(gw: Word, gws: &WSet, aws: &WSet,
 }
 
 // get upper bound for mean guesses at state
-fn solve_state(gws: &WSet, aws: &WSet, n: i32,
-							ht: &HTable) -> Option<DTree> {
+fn solve_state(gws: &WSet, aws: &WSet, n: i32, hd: &mut HData) -> Option<DTree> {
 	// worth?
 	// if aws.len() == 1 {
 		// // 100% chance for one guess
@@ -123,6 +132,10 @@ fn solve_state(gws: &WSet, aws: &WSet, n: i32,
 			// fbmap: [(fb_solved, DTree::Leaf)].into()
 		// });
 
+	if n == 0 {
+		return None
+	}
+
 	// todo update comments for no above?
 	let mut dt = Some(DTree::Leaf);
 	let mut eval = f64::INFINITY;
@@ -130,13 +143,14 @@ fn solve_state(gws: &WSet, aws: &WSet, n: i32,
 	// in "endgame", check if guessing a possible
 	// answer guarantees correct next guess (score < 2)
 	// (so far aws.len() = 14 is max i've found where this is possible)
-	if aws.len() < 10 {
+	if aws.len() <= ENDGCUTOFF {
 		for aw in aws {
-			match solve_given(*aw, gws, aws, n, ht) {
+			match solve_given(*aw, gws, aws, n, hd) {
 				None => {},
 				Some(dt2) => {
 					let eval2 = dt2.get_eval();
 					if eval2 < 2.0 {
+						hd.record(aws.len(), eval2); 
 						return Some(dt2);
 					} else if eval2 < eval {
 						dt = Some(dt2);
@@ -148,14 +162,15 @@ fn solve_state(gws: &WSet, aws: &WSet, n: i32,
 	}
 	// dont bother checking other words if a 2 was found
 	if eval < 2.001 {
+		hd.record(aws.len(), eval); 
 		return dt;
 	}
 
 	// search top heuristic words and stop
 	// on guaranteed next guess (score = 2)
-	for gw in top_words(gws, aws, ht, 5) {
+	for gw in top_words(gws, aws, hd, NTOPS) {
 		// println!("{}", gw.to_string());
-		match solve_given(gw, gws, aws, n, ht) {
+		match solve_given(gw, gws, aws, n, hd) {
 			None => {},
 			Some(dt2) => {
 				let eval2 = dt2.get_eval();
@@ -169,20 +184,29 @@ fn solve_state(gws: &WSet, aws: &WSet, n: i32,
 		};
 	}
 
-	return dt;
+	if eval == f64::INFINITY {
+		// todo why inf?
+		hd.record_inf(n as usize);
+		return None;
+	} else {
+		hd.record(aws.len(), eval);
+		return dt;
+	}
 }
 
-// best found: salet, 3.425917926565874
+// best found: salet, 3.425917926565871
 fn main() {
 	let gws = get_words("data/guess_words").unwrap();
 	let aws = get_words("data/answer_words").unwrap();
-	let ht = read_heuristic("data/heuristic.csv").unwrap();
+	let mut hd = HData::new();
+	hd.read("data/happrox.csv").unwrap();
 	let w = Word::from(&String::from("salet")).unwrap();
-	// why is it displaying bad ones???
-	// is it bc heuristic table didnt estimate above?
-	// for (i,gw) in top_words(&gws, &aws, &ht, 100).iter().enumerate() {
+	// good way to generate hdata
+	// for (i,gw) in top_words(&gws, &aws, &hd, 100).iter().enumerate() {
 		// println!("{}. {}", i+1, gw.to_string()); 
+		// solve_given(*gw, &gws, &aws, 6, &mut hd);
 	// }
-	println!("{:?}", solve_given(w, &gws, &aws, 5, &ht));
-	// write_data(&"data/data.csv", &pts).unwrap();
+	let dt = solve_given(w, &gws, &aws, 6, &mut hd).unwrap();
+	println!("{}", dt.get_eval())
+	// hd.write("data/hdata.csv").unwrap();
 }
