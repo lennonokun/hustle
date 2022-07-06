77b842b53cc06a94429c8864e88125d1fad9d645
Lennon Okun <lennonokun@gmail.com>
2022-07-06 11:39:02 -0400

    formatting

--- src/solve/analysis.rs
+++ src/solve/analysis.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write, Result};
+use std::io::{BufRead, BufReader, Result, Write};
 use std::path::Path;
 use std::sync::Mutex;
 
@@ -9,22 +9,26 @@ use crate::ds::*;
 // let ht[0] be zero to make indexing easier, so +1
 // does this really need to be f64
 pub struct HData {
-	approx: [f64; NWORDS+1],
+  approx: [f64; NWORDS + 1],
 }
 
 impl HData {
-	pub fn load<P>(p: P) -> Result<Self> where P: AsRef<Path> {
-		let file = File::open(p)?;
-		let reader = BufReader::new(file);
-		let approx: [f64; NWORDS+1] = reader.lines()
-			.filter_map(|s| s.ok()?.parse::<f64>().ok())
-			.collect::<Vec<f64>>()
-			.try_into().expect("expected NWORDS+1 lines in heuristic cache");
-		Ok(Self{approx})
-	}
+  pub fn load<P>(p: P) -> Result<Self>
+  where
+    P: AsRef<Path>, {
+    let file = File::open(p)?;
+    let reader = BufReader::new(file);
+    let approx: [f64; NWORDS + 1] = reader
+      .lines()
+      .filter_map(|s| s.ok()?.parse::<f64>().ok())
+      .collect::<Vec<f64>>()
+      .try_into()
+      .expect("expected NWORDS+1 lines in heuristic cache");
+    Ok(Self { approx })
+  }
 
-	#[inline]
-	pub fn get_approx(&self, n: usize) -> f64 {
-		self.approx[n]
-	}
+  #[inline]
+  pub fn get_approx(&self, n: usize) -> f64 {
+    self.approx[n]
+  }
 }
