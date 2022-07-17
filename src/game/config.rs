d01f66bfd827fec2440dc65e555027bec6e24b18
Lennon Okun <lennonokun@gmail.com>
2022-07-17 15:58:50 -0400

    replaced toml_loader with config-rs

--- src/game/config.rs
+++ src/game/config.rs
@@ -1,60 +1,73 @@
-#![feature(struct_field_attributes)]
+use config::{Config as ConfigMod, File};
 use lazy_static::lazy_static;
-use std::fs::File;
 use std::collections::HashMap;
 use std::io::{self, Read};
 use std::error::Error;
 use std::path::{Path, PathBuf};
 use std::env;
-use toml_loader::{self,Loadable};
+use std::default::Default;
 
 use serde::Deserialize;
-use toml;
 use cursive::theme::Color;
 
 lazy_static! {
-  pub static ref CONFIG: Config = {Config::find()};
+  // find config on start up
+  pub static ref CONFIG: Config = {Config::find().unwrap()};
 }
 
-pub trait Loadable {
-  fn load(vec: Vec<&Path>) -> Option<Self> where Self: Sized;
-}
-
-#[derive(Loadable, Debug)]
 pub struct Config {
-  #[color]
+  // pub feedback_theme: FeedbackTheme,
   pub feedback_fg: Color,
-  #[color]
   pub feedback_absent_bg: Color,
-  #[color]
   pub feedback_present_bg: Color,
-  #[color]
   pub feedback_correct_bg: Color,
-  #[color]
   pub impossible_fg: Color,
   pub word_banks: HashMap<String, String>,
 }
 
+// loader config
+#[derive(Deserialize)]
+struct RawConfig {
+  // pub feedback_theme: Option<LFeedbackTheme>,
+  pub feedback_fg: String,
+  pub feedback_absent_bg: String,
+  pub feedback_present_bg: String,
+  pub feedback_correct_bg: String,
+  pub impossible_fg: String,
+  pub word_banks: HashMap<String, String>,
+}
+
+macro_rules! add_src {
+  ($builder: expr, $($buf: expr),+) => {
+    let mut pb = PathBuf::new();
+    $(pb.push($buf));+;
+    $builder = $builder.add_source(File::from(pb))
+  };
+}
+
 impl Config {
-  pub fn find() -> Self {
-    let p1 = Path::new("/usr/share/hustle/config.toml");
-
-    // try to do xdg merged with defaults
-    if let Ok(xdg) = env::var("XDG_CONFIG_HOME") {
-      let pb: PathBuf = [xdg, "hustle/config.toml".into()].iter().collect();
-      let p2 = pb.as_path();
-      if p2.exists() {return Config::load(vec![p2, p1]).unwrap()}
+  pub fn find() -> Option<Self> {
+    // default < home < xdg
+    let mut builder = ConfigMod::builder();
+    add_src!(builder, "/usr/share/hustle/config.toml");
+    if let Ok(homep) = env::var("HOME") {
+      add_src!(builder, homep, ".config/hustle/config.toml");
+    } if let Ok(xdgp) = env::var("XDG_CONFIG_HOME") {
+      add_src!(builder, xdgp, "hustle/config.toml");
     }
 
-    // try to do xdg merged with defaults
-    if let Ok(hp) = env::var("HOME") {
-      let pb: PathBuf = [hp, ".config/hustle/config.toml".into()].iter().collect();
-      let p2 = pb.as_path();
-      if p2.exists() {return Config::load(vec![p2, p1]).unwrap()}
-    }
+    let rawcfg: RawConfig = builder
+      .build().expect("couldn't build config")
+      .try_deserialize().ok()?;
 
-    // just use defaults
-    return Config::load(vec![p1]).unwrap();
+    Some(Config {
+      feedback_fg: Color::parse(&rawcfg.feedback_fg)?,
+      feedback_absent_bg: Color::parse(&rawcfg.feedback_absent_bg)?,
+      feedback_present_bg: Color::parse(&rawcfg.feedback_present_bg)?,
+      feedback_correct_bg: Color::parse(&rawcfg.feedback_correct_bg)?,
+      impossible_fg: Color::parse(&rawcfg.impossible_fg)?,
+      word_banks: rawcfg.word_banks,
+    })
   }
 }
 
@@ -63,8 +76,7 @@ mod test {
   use super::*;
 
   #[test]
-  pub fn default_config() {
-    let cfg = Config::load(vec![Path::new("/usr/share/hustle/config.toml")]);
-    assert!(cfg.is_some());
+  pub fn find_some() {
+    assert!(Config::find().is_some());
   }
 }
