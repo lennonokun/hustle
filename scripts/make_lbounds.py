ab4fb76daa9de382852bea2bb4aac97a1a229c57
Lennon Okun <lennonokun@gmail.com>
2022-08-01 11:46:02 -0400

    improve lower bounds

--- scripts/make_lbounds.py
+++ scripts/make_lbounds.py
@@ -10,13 +10,15 @@ import matplotlib.pyplot as plt
 @click.argument("f_plot")
 def cli(f_ldata, step, f_out, f_plot):
   df = pd.DataFrame(pd.read_csv(f_ldata, comment="#"))
-  df = df.sort_values("alen").reset_index()
+  df = df.sort_values("alen").reset_index(drop=True)
 
   # process
   a = df.loc[0, "alen"]
   b = df.loc[len(df)-1, "alen"]
   alens = np.arange(a, b+1)
-  lbs = df.lb.to_numpy().repeat(step)[:-(step-1)]
+  lbs = df.lb.to_numpy()
+  if step > 1:
+    lbs = lbs.repeat(step)[:-(step-1)]
   
   df_out = pd.DataFrame({"alen": alens, "lb": lbs})
   df_out.to_csv(f_out, index=False)
