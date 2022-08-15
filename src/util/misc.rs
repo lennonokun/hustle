0fef3163331d5726a8e635d75329f5004b1594cd
Lennon Okun <lennonokun@gmail.com>
2022-08-15 14:40:56 -0400

    increase max word length

--- src/util/misc.rs
+++ src/util/misc.rs
@@ -2,7 +2,7 @@
 pub const DEFTURNS: u32 = 6;
 pub const NEXTRA: usize = 5;
 pub const DEFWLEN: u8 = 5;
-pub const MAXWLEN: usize = 11;
+pub const MAXWLEN: usize = 15;
 
 pub const DEFWBP: &'static str = "/usr/share/hustle/bank1.csv";
 pub const DEFWBP2: &'static str = "/usr/share/hustle/bank2.csv";
